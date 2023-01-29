71c6737ba4f89f533a04ef787559c81b1e8863cb
CPlusMinus2000 <hcolin88@gmail.com>
2023-01-29 16:47:54 -0500

    modified print output slightly

--- k1/user_tasks_k1.cc
+++ k1/user_tasks_k1.cc
@@ -9,25 +9,25 @@ extern "C" void Task_0()
 {
     while (1)
     {
-        char msg[] = "Entered into user task 0\r\n";
+        char msg[] = "entered into user task 0\r\n";
         uart_puts(0, 0, msg, sizeof(msg) - 1);
         Create(2, &Sub_Task);
-        char msg1[] = "Created: task 1\r\n";
+        char msg1[] = "created: task 1\r\n";
         uart_puts(0, 0, msg1, sizeof(msg1) - 1);
 
         Create(2, &Sub_Task);
-        char msg2[] = "Created: task 2\r\n";
+        char msg2[] = "created: task 2\r\n";
         uart_puts(0, 0, msg2, sizeof(msg2) - 1);
 
         Create(0, &Sub_Task);
-        char msg3[] = "Created: task 3\r\n";
+        char msg3[] = "created: task 3\r\n";
         uart_puts(0, 0, msg3, sizeof(msg3) - 1);
 
         Create(0, &Sub_Task);
-        char msg4[] = "Created: task 4\r\n";
+        char msg4[] = "created: task 4\r\n";
         uart_puts(0, 0, msg4, sizeof(msg4) - 1);
 
-        char msg5[] = "FirstUserTask: exiting \r\n";
+        char msg5[] = "exiting task 0\r\n";
         uart_puts(0, 0, msg5, sizeof(msg5) - 1);
         Exit();
     }
@@ -39,14 +39,14 @@ extern "C" void Sub_Task()
     int p_id = MyParentTid();
     print("my task id: ", 12);
     print_int(id);
-    print(" my parent id: ", 15);
+    print("; my parent id: ", 16);
     print_int(p_id);
     print("\r\n", 2);
 
     Yield();
     print("my task id: ", 12);
     print_int(id);
-    print(" my parent id: ", 15);
+    print("; my parent id: ", 16);
     print_int(p_id);
     print("\r\n", 2);
 
