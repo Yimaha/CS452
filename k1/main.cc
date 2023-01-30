ea7907ece9204e4b902122bc798f3345bcf37d6b
CPlusMinus2000 <hcolin88@gmail.com>
2023-01-30 17:26:20 -0500

    timer functions imported

--- k1/main.cc
+++ k1/main.cc
@@ -6,36 +6,37 @@
 
 #define NL uart_puts(0, 0, "\r\n", 2)
 
-extern char __bss_start, __bss_end;                    // defined in linker script
+extern char __bss_start, __bss_end;					   // defined in linker script
 extern uintptr_t __init_array_start, __init_array_end; // defined in linker script
 typedef void (*funcvoid0_t)();
 
-extern "C" void kmain()
-{
-    char m1[] = "init kernel\r\n";
-    uart_puts(0, 0, m1, sizeof(m1) - 1);
-    Kernel kernel = Kernel();
-    char m2[] = "finished kernel init, started scheduling user tasks\r\n";
+extern "C" void kmain() {
+	char m1[] = "init kernel\r\n";
+	uart_puts(0, 0, m1, sizeof(m1) - 1);
+	Kernel kernel = Kernel();
+	char m2[] = "finished kernel init, started scheduling user tasks\r\n";
 
 	uart_puts(0, 0, m2, sizeof(m2) - 1);
 	for (;;) // infinite loop, kernel never needs to exit until killed by power switch
 	{
-		kernel.schedule_next_task(); // tell kernel to schedule next task
-		kernel.activate();           // tell kernel to activate the scheduled task
-		kernel.handle();             // tell kernel to handle the request from task
+		kernel.schedule_next_task();			  // tell kernel to schedule next task
+		kernel.activate();						  // tell kernel to activate the scheduled task
+		kernel.handle();						  // tell kernel to handle the request from task
 		char m3[] = "completed kernel cycle\r\n"; // logging that is useful, but should be removed later
 		uart_puts(0, 0, m3, sizeof(m3) - 1);
 	}
 }
 
-int main()
-{
+int main() {
 	init_gpio();
 	init_spi(0);
 	init_uart(0);
 
 	for (funcvoid0_t* ctr = (funcvoid0_t*)&__init_array_start; ctr < (funcvoid0_t*)&__init_array_end; ctr += 1)
 		(*ctr)();
+
+	// Let's try playing with the timer
+	print_int(clo());
 
 	kmain(); // where the actual magic happens
 	return 0;
--- k1/rpi.h
+++ k1/rpi.h
@@ -10,10 +10,17 @@ char uart_getc(size_t spiChannel, size_t uartChannel);
 void uart_putc(size_t spiChannel, size_t uartChannel, char c);
 void uart_puts(size_t spiChannel, size_t uartChannel, const char* buf, size_t blen);
 
+// Timer Functions
+uint32_t clo(void);
+uint32_t chi(void);
+uint64_t time(void);
+void set_comparator(uint32_t reg_num, uint32_t interrupt_time);
+
 // anything that can be invoked from assembly and is useful goes here
 extern "C" void* memset(void* s, int c, size_t n);
 extern "C" void* memcpy(void* __restrict__ dest, const void* __restrict__ src, size_t n);
 extern "C" void val_print(uint64_t c);
 extern "C" void print_exception();
+extern "C" void crash(void);
 extern "C" void assert_crash(const char* msg = nullptr, const size_t len = 0);
 void kernel_assert(bool cond, const char* msg = nullptr, const size_t len = 0);
